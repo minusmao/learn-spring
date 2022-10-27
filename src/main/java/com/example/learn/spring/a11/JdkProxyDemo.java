843854a657b37b10a517b514c50157946727818e
Minus <trice12345@aliyun.com>
2022-10-28 00:43:11 +0800

    feat: 第十一讲-AOP实现之agent类加载

---
+++ src/main/java/com/example/learn/spring/a11/JdkProxyDemo.java
@@ -0,0 +1,45 @@
+package com.example.learn.spring.a11;
+import java.io.IOException;
+import java.lang.reflect.Proxy;
+
+public class JdkProxyDemo {
+
+    interface Foo {
+        void foo();
+    }
+
+    static final class Target implements Foo {
+        public void foo() {
+            System.out.println("target foo");
+        }
+    }
+
+    /*
+      jdk代理的特点：1、代理对象和目标对象不能互相强制类型转换，只能转换成他们共同实现的接口
+                   2、目标对象可以为final类型
+     */
+
+    // jdk 只能针对接口代理
+    // cglib
+    public static void main(String[] param) throws IOException {
+        // 目标对象
+        Target target = new Target();
+
+        // note: 代理类是运行期间动态生成的，所以需要用到ClassLoader把动态生成的字节码加载成对象
+        ClassLoader loader = JdkProxyDemo.class.getClassLoader(); // 用来加载在运行期间动态生成的字节码
+        Foo proxy = (Foo) Proxy.newProxyInstance(loader, new Class[]{Foo.class}, (p, method, args) -> {
+            System.out.println("before...");
+            // 目标.方法(参数)
+            // 方法.invoke(目标, 参数);
+            Object result = method.invoke(target, args);
+            System.out.println("after....");
+            return result; // 让代理也返回目标方法执行的结果
+        });
+
+        System.out.println(proxy.getClass());
+
+        proxy.foo();
+
+        System.in.read();
+    }
+}
