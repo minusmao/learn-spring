9ff898f52152f4e279974c1f0dcbd5a8a894946b
Minus <trice12345@aliyun.com>
2022-11-04 00:40:39 +0800

    feat: 第廿五讲-控制器方法执行流程（补充完整流程笔记）

--- src/main/java/com/example/learn/spring/a25/A25.java
+++ src/main/java/com/example/learn/spring/a25/A25.java
@@ -85,6 +85,35 @@ public class A25 {
                                        ----> 添加model数据、处理视图名、是否渲染到ModelAndViewContainer
          ServletInvocableHandlerMethod ----> 从ModelAndViewContainer中拿到最终的ModelAndView对象，并返回给RequestMappingHandlerAdapter
 
+         补充：参考（https://blog.csdn.net/qq_30056341/article/details/113488860）
+              客户端请求 -> DispatcherServlet: doService() -> doDispatch()
+              -> RequestMappingHandlerAdapter: handle() -> handleInternal() -> invokeHandlerMethod()
+              -> ServletInvocableHandlerMethod: invokeAndHandle()
+
+              RequestMappingHandlerAdapter的invokeHandlerMethod()方法做了大致以下事情：
+                  * 创建WebDataBinderFactory
+                  * 创建ModelFactory
+                  * 创建ServletInvocableHandlerMethod
+                  * 给ServletInvocableHandlerMethod设置HandlerMethodArgumentResolver
+                     (在RequestMappingHandlerAdapter的afterPropertiesSet()方法中会初始化SpringMVC内置的适配各种类型请求参数的解析器HandlerMethodArgumentResolver)
+                  * 给ServletInvocableHandlerMethod设置DataBinderFactory
+                  * 给ServletInvocableHandlerMethod设置ParameterNameDiscoverer
+                  * 创建ModelAndViewContainer
+                  * 调用ServletInvocableHandlerMethod的invokeAndHandle()方法
+
+
+              ServletInvocableHandlerMethod: invokeAndHandle() -> invokeForRequest()执行并获得returnValue
+              invokeForRequest()顺序执行：getMethodArgumentValues()解析和绑定请求参数、完成请求参数的绑定、校验和类型转换工作
+                                        doInvoke() 基于反射正式开始调用目标处理请求方法执行(HandlerMethod)
+
+              getMethodArgumentValues() -> HandlerMethodArgumentResolverComposite: resolveArgument()即调用参数解析器解析参数
+
+              拿ModelAttributeMethodProcessor参数解析器举例，其resolveArgument()方法内部：
+                  * createAttribute() 基于反射获取目标处理方法的参数类型并初始化模型参数实例
+                  * 调用binderFactory.createBinder() 创建请求参数绑定器WebRequestDataBinder
+                  * bindRequestParameters() 开始绑定请求参数到目标方法中
+                  * validateIfApplicable() 校验请求参数是否符合注解@Validate定义的校验规则，如果请求参数没有使用@Validate系列注解声明校验规则，则不会进行校验
+
     */
 
 }
