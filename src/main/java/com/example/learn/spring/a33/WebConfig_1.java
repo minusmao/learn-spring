c82fda8c196cce748ae5dd7064d4da794bb1f6e7
Minus <trice12345@aliyun.com>
2022-11-06 21:48:50 +0800

    feat: 第卅三讲-BeanNameUrlHandlerMapping与SimpleControllerHandlerAdapter

---
+++ src/main/java/com/example/learn/spring/a33/WebConfig_1.java
@@ -0,0 +1,113 @@
+package com.example.learn.spring.a33;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletRegistrationBean;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.*;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Configuration
+public class WebConfig_1 {
+    @Bean // ⬅️内嵌 web 容器工厂
+    public TomcatServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory(8080);
+    }
+
+    @Bean // ⬅️创建 DispatcherServlet
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
+    @Bean // ⬅️注册 DispatcherServlet, Spring MVC 的入口
+    public DispatcherServletRegistrationBean servletRegistrationBean(DispatcherServlet dispatcherServlet) {
+        return new DispatcherServletRegistrationBean(dispatcherServlet, "/");
+    }
+
+    // 仿造BeanNameUrlHandlerMapping功能
+    // /c1  -->  /c1
+    // /c2  -->  /c2
+    @Component
+    static class MyHandlerMapping implements HandlerMapping {
+        // 处理请求
+        @Override
+        public HandlerExecutionChain getHandler(HttpServletRequest request) throws Exception {
+            String key = request.getRequestURI();
+            Controller controller = collect.get(key);
+            if (controller == null) {
+                return null;
+            }
+            return new HandlerExecutionChain(controller);  // 执行链
+        }
+
+        @Autowired
+        private ApplicationContext context;
+        private Map<String, Controller> collect;  // 路径和Controller映射关系
+
+        @PostConstruct
+        public void init() {
+            collect = context.getBeansOfType(Controller.class).entrySet()
+                    .stream().filter(e -> e.getKey().startsWith("/"))  // 找到容器中所有beanName为"/"开头的Controller
+                    .collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue()));
+            System.out.println(collect);
+        }
+    }
+
+    // 仿造SimpleControllerHandlerAdapter功能
+    @Component
+    static class MyHandlerAdapter implements HandlerAdapter {
+
+        @Override
+        public boolean supports(Object handler) {
+            return handler instanceof Controller;
+        }
+
+        @Override
+        public ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+            if (handler instanceof Controller controller) {
+                controller.handleRequest(request, response);
+            }
+            return null;
+        }
+
+        @Override  // 过时，处理重定向
+        public long getLastModified(HttpServletRequest request, Object handler) {
+            return -1;
+        }
+    }
+
+    @Component("/c1")
+    public static class Controller1 implements Controller {
+        @Override
+        public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+            response.getWriter().print("this is c1");
+            return null;
+        }
+    }
+
+    @Component("c2")
+    public static class Controller2 implements Controller {
+        @Override
+        public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+            response.getWriter().print("this is c2");
+            return null;
+        }
+    }
+
+    @Bean("/c3")
+    public Controller controller3() {
+        return (request, response) -> {
+            response.getWriter().print("this is c3");
+            return null;
+        };
+    }
+}
